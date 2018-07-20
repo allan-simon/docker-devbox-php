d36e9f45dc372ebc5348814f60e901bd45f98fef
Allan Simon <allan.simon@supinfo.com>
2018-07-20 15:54:16 +0200

    Update change_user_uid.sh

--- change_user_uid.sh
+++ change_user_uid.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-usermod -u $HOST_USER_UID vagrant
-chown -R vagrant /home/vagrant
+
+# if needed, we change the uid of the vagrant user
+# so that to be sure it matches the one of the host
+# this way, changing a file from within the container
+# does not alter ownership
+if ! [ $(id -u vagrant) = $HOST_USER_UID ]; then
+    usermod -u $HOST_USER_UID vagrant
+fi
 if  test -z "$@" ; then
     /sbin/my_init
 else 
